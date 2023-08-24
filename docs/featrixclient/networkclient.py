f5f83462c0a3dbd7143ea702a416243d6a75d314
Mitch Haile <mitch.haile@gmail.com>
2023-08-24 12:36:18 -0400

    Mock pandas

--- docs/featrixclient/networkclient.py
+++ docs/featrixclient/networkclient.py
@@ -14,28 +14,15 @@ import time
 import traceback
 import uuid
 
-import pandas as pd
+#import pandas as pd
 import requests
 
-logger = logging.getLogger("featrix-client")
-# logger.basicConfig(format='%(asctime)s %(message)s', level=logger.DEBUG)
+from unittest import mock
 
-# Create handlers
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler("featrix-client.log")
-c_handler.setLevel(logging.WARNING)
-f_handler.setLevel(logging.ERROR)
-
-# Create formatters and add it to handlers
-c_format = logging.Formatter("featrix %(name)s - %(levelname)s - %(message)s")
-f_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
-
-# Add handlers to the logger
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
-
+# Mock open3d because it fails to build in readthedocs
+MOCK_MODULES = ["pd"]
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 __version__ = "<no version information available>"
 
